eb02442a4ce881c7803671762a94894140ded547
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-01-20 23:59:04 +0700

    setup database tables

---
+++ database/migrations/2021_01_20_160044_create_practicum_videos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePracticumVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('practicum_videos', function (Blueprint $table) {
+            $table->foreignId('practicum_modules');
+            $table->string('video_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('practicum_videos');
+    }
+}
