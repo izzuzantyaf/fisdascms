f483ea723d8a1c4811e6a32440c7ad83cea7db5a
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-01-25 11:30:04 +0700

    remove unused code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,18 +6,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title','Dashboard') | Fisdas CMS</title>
   <link rel="stylesheet" href="/css/app.css">
-  <!-- <script defer src="/node_modules/@fortawesome/fontawesome-free/js/brands.js"></script>
-  <script defer src="/node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
-  <script defer src="/node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script> -->
   <script defer type="text/javascript" src="/js/app.js"></script>
 </head>
 
 <body>
 
-  @include('components.navbar')
+  <x-navbar />
 
   <div class="flex border h-screen">
-    @include('components.sidebar')
+    <x-sidebar />
     @yield('content')
   </div>
 
