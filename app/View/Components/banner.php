d421cbb04979dd787c6baef5e837a237a07486ec
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-01-25 11:29:27 +0700

    nothing is changed

--- app/View/Components/banner.php
+++ app/View/Components/banner.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class banner extends Component
+class Banner extends Component
 {
     /**
      * Create a new component instance.
