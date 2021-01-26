6755087af4b1755a04dd5bb7356c9847acbda4ea
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-01-26 08:54:02 +0700

    fixing again

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,24 @@
+<nav class="bg-blue-700 h-16">
+    <div class="container relative flex items-center justify-between mx-auto px-4 lg:justify-end h-16">
+
+        <!-- Mobile menu button-->
+        <button class="hamburger-menu lg:hidden inline-flex items-center justify-center p-2 rounded-md text-blue-400 hover:text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
+            <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+        </button>
+
+        <button class="avatar bg-blue-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-800 focus:ring-white" id="user-menu" aria-haspopup="true">
+            <span class="sr-only">Open user menu</span>
+            <div class="h-8 w-8 rounded-full text-white flex items-center justify-center">
+                W
+            </div>
+        </button>
+
+        <div class="profile-dropdown origin-top-right absolute right-4 mt-40 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 transition ease-in-out duration-200 transform scale-0" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+            <a href="/admin-profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100" role="menuitem">Your Profile</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100" role="menuitem">Settings</a>
+            <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100" role="menuitem">Sign out</a>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,27 @@
+<aside class="sidebar z-20 w-64 overflow-y-auto bg-gray-100 dark:bg-gray-800 flex-shrink-0 h-full absolute top-0 bottom-0 lg:relative transition ease-in-out duration-500 transform -translate-x-72 lg:translate-x-0">
+    <div class="py-4 text-gray-500 dark:text-gray-400">
+        <div class="sidebar-header flex flex-col-reverse px-6">
+            <a class="text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
+                Admin panel
+            </a>
+            <div class="back-btn cursor-pointer lg:hidden self-end"><i class="fas fa-arrow-left"></i></div>
+        </div>
+
+        <div class="menu-list mt-6">
+            @foreach($menu_list as $menu)
+            <a href="{{ $menu['route'] }}">
+                <div class="relative px-6 py-3 inline-flex items-center w-full text-sm font-semibold text-gray-500 transition-colors duration-150 hover:text-blue-700 dark:hover:text-gray-200 dark:text-gray-100">
+                    @if(false)
+                    <span class="absolute inset-y-0 left-0 w-1 bg-blue-700 rounded-tr-lg rounded-br-lg" aria-hidden="true"></span>
+                    @endif
+                    <div class="menu-icon w-12">
+                        <i class="{{ $menu['icon'] }} text-2xl"></i>
+                    </div>
+                    <span class="ml-4">{{ $menu['name'] }}</span>
+                </div>
+            </a>
+            @endforeach
+        </div>
+
+    </div>
+</aside>
