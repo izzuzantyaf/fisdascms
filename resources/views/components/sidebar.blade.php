17ad974f9f815c46142535868f265e923a743562
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-01-27 17:00:39 +0700

    make sidebar fixed

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
-<aside class="sidebar z-20 w-64 overflow-y-auto bg-gray-100 dark:bg-gray-800 flex-shrink-0 h-full absolute top-0 bottom-0 lg:relative transition ease-in-out duration-500 transform -translate-x-72 lg:translate-x-0">
+<aside class="sidebar z-20 w-64 overflow-y-auto bg-gray-100 dark:bg-gray-800 flex-shrink-0 fixed top-0 bottom-0 lg:relative transition ease-in-out duration-500 transform -translate-x-72 lg:translate-x-0">
     <div class="py-4 text-gray-500 dark:text-gray-400">
         <div class="sidebar-header flex flex-col-reverse px-6">
-            <a class="text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
+            <a class="text-xl font-bold text-gray-800 dark:text-gray-200" href="#">
                 Admin panel
             </a>
             <div class="back-btn cursor-pointer lg:hidden self-end"><i class="fas fa-arrow-left"></i></div>
