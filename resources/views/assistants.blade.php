6f94ca29348b430039e561c120d95b4ba3e800af
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-01-28 22:03:27 +0700

    code refactoring

--- resources/views/assistants.blade.php
+++ resources/views/assistants.blade.php
@@ -2,72 +2,63 @@
 @section('title','Asisten')
 
 @section('content')
-<div class="assistants overflow-x-auto flex-grow p-8">
+<div class="assistants overflow-x-hidden flex-grow p-8">
   <h1 class="text-4xl font-bold">Asisten</h1>
-  <div class="mt-8 overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 align-middle sm:px-6 lg:px-8">
-      <div class="shadow border-b border-gray-200 sm:rounded-lg">
-        <table class="w-full divide-y divide-gray-200">
-          <thead class="bg-gray-50">
-            <tr>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Nama
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Kode
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Gender
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Tingkat
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Link feedback
-              </th>
-              <th scope="col" class="relative px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
-            <?php foreach ($assistants as [
-              'name' => $name,
-              'code' => $code,
-              'gender' => $gender,
-              'standing' => $standing,
-              'feedback_link' => $feedback_link,
-            ]) { ?>
-              <tr>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="flex items-center">
-                    <div class="text-sm font-medium text-gray-900">
-                      <?= $name ?>
-                    </div>
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm text-gray-900"><?= $code ?></div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm <?= $gender === 'male' ? 'text-blue-500' : 'text-pink-500' ?>">
-                  <?= $gender ?>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  <?= $standing ?>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  <?= $feedback_link ?>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                </td>
-              </tr>
-            <?php } ?>
-            <!-- More items... -->
-          </tbody>
-        </table>
-      </div>
-    </div>
+  <div class="w-full mt-8 overflow-x-auto shadow border border-gray-200 rounded-lg">
+    <table class="divide-y divide-gray-200">
+      <thead class="bg-gray-50">
+        <tr class="text-left text-xs font-medium text-gray-500 uppercase">
+          <th scope="col" class="px-6 py-3 tracking-wider">
+            Nama
+          </th>
+          <th scope="col" class="px-6 py-3 tracking-wider">
+            Kode
+          </th>
+          <th scope="col" class="px-6 py-3 tracking-wider">
+            Gender
+          </th>
+          <th scope="col" class="px-6 py-3 tracking-wider">
+            Tingkat
+          </th>
+          <th scope="col" class="px-6 py-3 tracking-wider">
+            Link feedback
+          </th>
+          <th scope="col" class="px-6 py-3">
+            <span class="sr-only">Edit</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody class="bg-white divide-y divide-gray-200">
+        <?php foreach ($assistants as [
+          'name' => $name,
+          'code' => $code,
+          'gender' => $gender,
+          'standing' => $standing,
+          'feedback_link' => $feedback_link,
+        ]) { ?>
+          <tr class="text-xs">
+            <td class="px-6 py-4 font-medium text-gray-900 capitalize">
+              <?= $name ?>
+            </td>
+            <td class="px-6 py-4 text-gray-900 uppercase">
+              <?= $code ?>
+            </td>
+            <td class="px-6 py-4 <?= $gender === 'male' ? 'text-blue-500' : 'text-pink-500' ?> capitalize">
+              <?= $gender ?>
+            </td>
+            <td class="px-6 py-4 text-gray-500 capitalize">
+              <?= $standing ?>
+            </td>
+            <td class="px-6 py-4 text-gray-500">
+              <?= $feedback_link ?>
+            </td>
+            <td class="px-6 py-4 text-right font-medium">
+              <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
   </div>
 </div>
 @endsection
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -18,7 +18,7 @@
         <div class="profile-dropdown origin-top-right absolute right-4 mt-40 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 transition ease-in-out duration-200 transform scale-0" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
             <a href="/admin-profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100" role="menuitem">Your Profile</a>
             <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100" role="menuitem">Settings</a>
-            <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100" role="menuitem">Sign out</a>
+            <a href="/logout" class="block px-4 py-2 text-sm text-red-500 hover:bg-blue-100" role="menuitem">Sign out</a>
         </div>
     </div>
 </nav>
