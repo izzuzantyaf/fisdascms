e316705901a64f259b737cd2f5f90941e26c5cc8
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-06-24 17:33:05 +0700

    code refactoring

--- resources/views/components/cloudinary-js.blade.php
+++ resources/views/components/cloudinary-js.blade.php
@@ -1,16 +1,14 @@
+<div class="cloudinary-container col-span-full h-screen overflow-hidden rounded-md"></div>
 <script type="text/javascript">
     const mediaLibrary = cloudinary.createMediaLibrary({
         cloud_name: 'hxquybrtx',
         api_key: '493334639281711',
         username: 'msi.fisdas@gmail.com',
-        // timestamp: Math.floor(Date.now() / 1000).toString(),
-        // signature: document.querySelector('input.signature').value,
+        inline_container: 'div.cloudinary-container',
     }, {
         insertHandler: () => {},
         showHandler: () => {},
         hideHandler: () => {},
     }, )
-    document.getElementById('cloudinary-ml').addEventListener('click', () => {
-        mediaLibrary.show()
-    })
+    mediaLibrary.show()
 </script>
