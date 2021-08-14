4a7b76c63779547eaed84c4c4d3f4450911b20e1
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-08-14 13:34:40 +0700

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title','Dashboard') | Fisdas CMS</title>
   <link rel="stylesheet" href="/css/app.css">
+  <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
   <!-- <script type="text/javascript" src="https://media-library.cloudinary.com/global/all.js"></script> -->
   <script defer type="text/javascript" src="/js/app.js"></script>
 </head>
