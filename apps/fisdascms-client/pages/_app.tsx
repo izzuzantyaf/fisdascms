8c8f953e63d113e217320ec45ca399d4444827e7
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-05-17 14:00:57 +0700

    install and setup fontawesome

--- apps/fisdascms-client/pages/_app.tsx
+++ apps/fisdascms-client/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons"
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+config.autoAddCss = false
+library.add(faUserSecret)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
