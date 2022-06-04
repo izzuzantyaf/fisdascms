509288245048aacb55c55ccccaddea88bf925511
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-06-04 11:08:51 +0700

    fix practicum module controller test

--- apps/fisdascms-server/src/use-cases/practicum-module/practicum-module.controller.spec.ts
+++ apps/fisdascms-server/src/use-cases/practicum-module/practicum-module.controller.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PracticumModuleController } from './practicum-module.controller';
+import { PracticumModuleModule } from './practicum-module.module';
 
 describe('PracticumModuleController', () => {
   let controller: PracticumModuleController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [PracticumModuleController],
+      imports: [PracticumModuleModule],
     }).compile();
 
-    controller = module.get<PracticumModuleController>(PracticumModuleController);
+    controller = module.get<PracticumModuleController>(
+      PracticumModuleController,
+    );
   });
 
   it('should be defined', () => {
