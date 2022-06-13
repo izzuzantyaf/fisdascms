c05a3845f3ec9641a6fa95ee5a15abdc602478fc
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-06-13 11:52:27 +0000

    optimize google fonts

--- apps/fisdascms-client/pages/_document.js
+++ apps/fisdascms-client/pages/_document.js
@@ -7,8 +7,7 @@ class MyDocument extends Document {
         <Head>
           <meta name="description" content="Website CMS Lab Fisika Dasar Tel-U" />
           <link rel="icon" href="/favicon.ico" />
-          <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
