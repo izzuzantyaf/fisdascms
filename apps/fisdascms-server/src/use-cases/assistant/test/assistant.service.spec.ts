054c572c26dbdcb5fce75a965678af3498c27f3c
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-08-04 13:46:49 +0700

    action: disable create and delete assistant func

--- apps/fisdascms-server/src/use-cases/assistant/test/assistant.service.spec.ts
+++ apps/fisdascms-server/src/use-cases/assistant/test/assistant.service.spec.ts
@@ -1,6 +1,6 @@
 import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { Assistant } from 'src/database/entity/assistant.entity';
+import { Assistant } from 'src/core/entities/assistant.entity';
 import { AssistantModule } from '../assistant.module';
 import { AssistantService } from '../assistant.service';
 
@@ -20,32 +20,32 @@ describe('AssistantService', () => {
     expect(service).toBeDefined();
   });
 
-  let assistantId;
-  describe('create()', () => {
-    it('harus berhasil menambahkan asisten', async () => {
-      const storedAssistant = await service.create({
-        name: 'Nama Asisten',
-        code: 'AAA',
-        phoneNumber: '082123456789',
-        lineId: 'random',
-        gender: 'male',
-        level: 'senior',
-        feedbackUrl: 'https://google.com',
-        profilePictureUrl: 'https://ikea.com',
-      });
-      assistantId = storedAssistant._id;
-      expect(storedAssistant).toBeInstanceOf(Assistant);
-    });
-  });
+  // let assistantId;
+  // describe('create()', () => {
+  //   it('harus berhasil menambahkan asisten', async () => {
+  //     const storedAssistant = await service.create({
+  //       name: 'Nama Asisten',
+  //       code: 'AAA',
+  //       phoneNumber: '082123456789',
+  //       lineId: 'random',
+  //       gender: 'male',
+  //       level: 'senior',
+  //       feedbackUrl: 'https://google.com',
+  //       profilePictureUrl: 'https://ikea.com',
+  //     });
+  //     assistantId = storedAssistant._id;
+  //     expect(storedAssistant).toBeInstanceOf(Assistant);
+  //   });
+  // });
 
-  describe('delete()', () => {
-    it('harus berhasil menghapus asisten', async () => {
-      expect(await service.delete(assistantId)).toBeInstanceOf(Assistant);
-    });
-    it('harus gagal menghapus asisten karena id tidak valid', async () => {
-      await expect(service.delete('salah')).rejects.toThrow(
-        BadRequestException,
-      );
-    });
-  });
+  // describe('delete()', () => {
+  //   it('harus berhasil menghapus asisten', async () => {
+  //     expect(await service.delete(assistantId)).toBeInstanceOf(Assistant);
+  //   });
+  //   it('harus gagal menghapus asisten karena id tidak valid', async () => {
+  //     await expect(service.delete('salah')).rejects.toThrow(
+  //       BadRequestException,
+  //     );
+  //   });
+  // });
 });
