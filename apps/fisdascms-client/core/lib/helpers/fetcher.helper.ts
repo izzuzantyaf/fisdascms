30382e66b1e1a3264607438b810145efc8ac1c37
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-10-29 21:59:33 +0700

    fix: adjust api endpoint url
    Remove NEXT_PUBLIC_SERVER_APP_BASEURL
    Change NEXT_PUBLIC_API_BASEURL to NEXT_PUBLIC_API_ENDPOINT_URL

--- apps/fisdascms-client/core/lib/helpers/fetcher.helper.ts
+++ apps/fisdascms-client/core/lib/helpers/fetcher.helper.ts
@@ -2,51 +2,40 @@ import { MyResponse } from "../../types/my-response.type"
 
 export const getFetch = async (path: string, query?: string) => {
   const url =
-    process.env.NEXT_PUBLIC_SERVER_APP_BASEURL +
-    path +
-    (query ? `?${query}` : "")
+    process.env.NEXT_PUBLIC_API_ENPOINT_URL + path + (query ? `?${query}` : "")
   const response = await fetch(url)
   return (await response.json()) as MyResponse
 }
 
 export const postFetch = async (path: string, body: object = {}) => {
-  const response = await fetch(
-    process.env.NEXT_PUBLIC_SERVER_APP_BASEURL + path,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    }
-  )
+  const response = await fetch(process.env.NEXT_PUBLIC_API_ENPOINT_URL + path, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
   return (await response.json()) as MyResponse
 }
 
 export const putFetch = async (path: string, body: object = {}) => {
-  const response = await fetch(
-    process.env.NEXT_PUBLIC_SERVER_APP_BASEURL + path,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    }
-  )
+  const response = await fetch(process.env.NEXT_PUBLIC_API_ENPOINT_URL + path, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
   return (await response.json()) as MyResponse
 }
 
 export const deleteFetch = async (path: string, body: object = {}) => {
-  const response = await fetch(
-    process.env.NEXT_PUBLIC_SERVER_APP_BASEURL + path,
-    {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    }
-  )
+  const response = await fetch(process.env.NEXT_PUBLIC_API_ENPOINT_URL + path, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
   return (await response.json()) as MyResponse
 }
